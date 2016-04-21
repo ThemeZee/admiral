2e4ae582ab5ddbcdb089fa47af2f646a22b71d15
Thomas W <netzberufler@googlemail.com>
2016-04-21 14:38:40 +0200

    added version numbers to wp_enqueue_style and wp_enqueue_script

--- functions.php
+++ functions.php
@@ -135,26 +135,28 @@ add_action( 'widgets_init', 'admiral_widgets_init' );
  * Enqueue scripts and styles.
  */
 function admiral_scripts() {
-	global $wp_scripts;
+
+	// Get Theme Version
+	$theme_version = wp_get_theme()->get( 'Version' );
 	
 	// Register and Enqueue Stylesheet
-	wp_enqueue_style( 'admiral-stylesheet', get_stylesheet_uri() );
+	wp_enqueue_style( 'admiral-stylesheet', get_stylesheet_uri(), array(), $theme_version );
 	
 	// Register Genericons
-	wp_enqueue_style( 'admiral-genericons', get_template_directory_uri() . '/css/genericons/genericons.css' );
+	wp_enqueue_style( 'admiral-genericons', get_template_directory_uri() . '/css/genericons/genericons.css', array(), '3.4.1' );
 	
 	// Register and Enqueue HTML5shiv to support HTML5 elements in older IE versions
-	wp_enqueue_script( 'admiral-html5shiv', get_template_directory_uri() . '/js/html5shiv.min.js', array(), '3.7.2', false );
-	$wp_scripts->add_data( 'admiral-html5shiv', 'conditional', 'lt IE 9' );
+	wp_enqueue_script( 'admiral-html5shiv', get_template_directory_uri() . '/js/html5shiv.min.js', array(), '3.7.3' );
+	wp_script_add_data( 'admiral-html5shiv', 'conditional', 'lt IE 9' );
 
 	// Register and enqueue navigation.js
-	wp_enqueue_script( 'admiral-jquery-navigation', get_template_directory_uri() .'/js/navigation.js', array('jquery') );
+	wp_enqueue_script( 'admiral-jquery-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20160421' );
 	
 	// Passing Parameters to navigation.js
 	wp_localize_script( 'admiral-jquery-navigation', 'admiral_menu_title', esc_html__( 'Navigation', 'admiral' ) );
 	
 	// Register and enqueue sidebar.js
-	wp_enqueue_script( 'admiral-jquery-sidebar', get_template_directory_uri() .'/js/sidebar.js', array('jquery') );
+	wp_enqueue_script( 'admiral-jquery-sidebar', get_template_directory_uri() . '/js/sidebar.js', array( 'jquery' ), '20160421' );
 	
 	// Register and Enqueue Google Fonts
 	wp_enqueue_style( 'admiral-default-fonts', admiral_google_fonts_url(), array(), null );
