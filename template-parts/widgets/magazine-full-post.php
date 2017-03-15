a2d59123e35515c3e110082deca724c2da78f873
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-03-15 10:37:33 +0100

    added new magazine templates for Pro widgets

---
+++ template-parts/widgets/magazine-full-post.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying full image posts in Magazine Post widgets
+ *
+ * @package Admiral
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?>>
+
+	<?php admiral_post_image(); ?>
+
+	<header class="entry-header">
+
+		<?php admiral_magazine_entry_meta(); ?>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+
+		<?php the_excerpt(); ?>
+
+	</div><!-- .entry-content -->
+
+</article>
